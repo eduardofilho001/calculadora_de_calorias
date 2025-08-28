a43dcb556caee30cde339f4fe62ba2b21bd0dba4
Eduardo Filho <eduardosmfilho01@gmail.com>
2025-08-28 20:20:06 +0100

    estilização, criação de tabelas,

--- calcular_calorias/views.py
+++ calcular_calorias/views.py
@@ -4,3 +4,22 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def resultado(request):
+    if request.method == "POST":
+        form = request.POST
+        if form.is_valida():
+            form.cleanned_data["comida"]
+            
+        #if form == ""
+        
+        Calorias = None
+        Carboidratos = None
+        Proteinas= None
+        Gorduras= None
+        Alcool= None
+    
+    #Calorias=(Carboidratos * 4)+(Proteinas * 4)+(Gorduras * 9)+(Alcool * 7)
+    
+        return render(request, "resultado.html")
+    return render(request, "resultado.html")
